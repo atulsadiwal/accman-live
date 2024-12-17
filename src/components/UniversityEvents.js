ed601e697776bd659cb4341c27ecda4317df0ff2
atulsadiwal <atul.sadiwal@brandshow.in>
2024-12-17 14:21:25 +0530

    changed

--- src/components/UniversityEvents.js
+++ src/components/UniversityEvents.js
@@ -11,7 +11,7 @@ const UniversityEvents = () => {
             University Events
           </h6>
           <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
-            <h2 className="text-3xl font-medium  text-gray-900 leading-snug font-playfair">
+            <h2 className="text-4xl font-medium  text-gray-900 leading-snug font-playfair">
               Bring The Best Speakers At<br className="hidden sm:block" /> Every Events
             </h2>
 
@@ -26,7 +26,7 @@ const UniversityEvents = () => {
           {/* First Column */}
           <div className="md:col-span-2 relative">
             <div
-              className="h-60 md:h-72 w-full bg-cover bg-center relative"
+              className="h-80 w-full bg-cover bg-center relative"
               style={{
                 backgroundImage: `url("/image/rear-view-of-audience-showing-hand.jpg")`,
                 filter: "brightness(60%)",
@@ -35,19 +35,19 @@ const UniversityEvents = () => {
               <div className="absolute inset-0 bg-purple-950 opacity-50"></div>
             </div>
             <div className="absolute top-0 left-0 p-6 text-white sm:pt-28 ">
-              <h3 className="text-md font-medium font-playfair mb-4">
+              <h3 className="text-xl font-medium font-playfair mb-4">
                 Educational Technology & Mobile Learning
               </h3>
-              <p className="text-[0.7rem] font-medium mb-4">
+              <p className="text-sm font-medium mb-4">
                 Explore the intersection of education and technology with a focus
                 on mobile learning. Enhance teaching methods through innovative
                 tools and digital resources for dynamic educational experiences.
               </p>
               <div className="flex space-x-4 items-center mb-4">
-                <span className="flex items-center text-[0.7rem]">
+                <span className="flex items-center text-sm">
                   <i className="mr-1"><FaLocationDot /></i> ACCMAN Auditorium
                 </span>
-                <span className="flex items-center text-[0.7rem]">
+                <span className="flex items-center text-sm">
                   <i className="mr-1"><FaCalendarAlt /></i> 9 June 2023
                 </span>
               </div>
@@ -70,18 +70,18 @@ const UniversityEvents = () => {
               />
             </div>
             <div className="py-4 px-5">
-              <h3 className="text-[0.75rem] tracking-wider text-indigo-900 font-playfair font-medium mb-4">
+              <h3 className="text-lg tracking-wider text-indigo-900 font-playfair font-medium mb-4">
                 Fintech & Key Investment Seminar
               </h3>
-              <p className="text-gray-900 text-[0.65rem]">
+              <p className="text-gray-900 text-sm">
                 Date: 19 December 2023
               </p>
-              <p className="text-gray-900 text-[0.65rem] mb-4">
+              <p className="text-gray-900 text-sm mb-4">
                 Location: ACCMAN Auditorium
               </p>
               <a
                 href="#"
-                className="text-indigo-900 uppercase tracking-widest text-[0.65rem] font-medium "
+                className="text-indigo-900 uppercase tracking-widest text-sm font-medium "
               >
                 Read More &rarr;
               </a>
@@ -98,18 +98,18 @@ const UniversityEvents = () => {
               />
             </div>
             <div className="p-4">
-              <h3 className="text-[0.75rem] tracking-wider text-indigo-900 font-playfair font-medium mb-4">
+              <h3 className="text-lg tracking-wider text-indigo-900 font-playfair font-medium mb-4">
                 Sport Management Information Webinar
               </h3>
-              <p className="text-gray-900 text-[0.65rem]">
+              <p className="text-gray-900 text-sm">
                 Date: 19 December 2023
               </p>
-              <p className="text-gray-900 text-[0.65rem] mb-4">
+              <p className="text-gray-900 text-sm mb-4">
                 Location: ACCMAN Grand Hall
               </p>
               <a
                 href="#"
-                className="text-indigo-900 uppercase tracking-widest text-[0.65rem] font-medium "
+                className="text-indigo-900 uppercase tracking-widest text-sm font-medium "
               >
                 Read More &rarr;
               </a>
