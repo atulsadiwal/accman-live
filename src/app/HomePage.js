144ae02a877039d0f196136983fd0e5ea149a93c
JatinBrandshow <jatin.yadav@brandshow.in>
2024-12-18 10:34:47 +0530

    New Changes

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -1,24 +1,17 @@
-import ContactUs from '@/components/ContactUs';
+import React from 'react';
 import Courses from '@/components/Courses';
 import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import HeroSection from '@/components/HeroSection';
-import HorizontalComponent from '@/components/HorizontalComponent';
 import JoinUsSection from '@/components/JoinUsSection';
-import KnowledgeComponent from '@/components/KnowledgeComponent';
 import MainSection from '@/components/MainSection';
 import NavBar from '@/components/NavBar';
 import NewsAndArticles from '@/components/NewsAndArticles';
 import Opportunities from '@/components/Opportunities';
 import OurCampus from '@/components/OurCampus';
 import TopCard from '@/components/TopCard';
-import University from '@/components/University';
 import UniversityEvents from '@/components/UniversityEvents';
 import WeAreTheOne from '@/components/WeAreTheOne';
-import WhyChoose from '@/components/WhyChoose';
 import WhyChooseUs from '@/components/WhyChooseUs';
 import YourJourney from '@/components/YourJourney';
-import React, { useState } from 'react';
 
 const HomePage = () => {
     return (
